e15cfc6d7453c6fdc18fd4386c3a4fae9820deb6
Semaeva <wunata95@gmail.com>
2023-02-09 00:18:19 +0600

    ready requirements

--- ufc/settings.py
+++ ufc/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3fv)h)io%i^!lcp%)c$=0-73fho$ij38le&l69_6*g#*fpq&ch'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
